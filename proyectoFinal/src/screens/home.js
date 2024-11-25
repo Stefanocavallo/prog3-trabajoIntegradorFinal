eb1df561e964ddac3e26ebdfbd0f2b89e689aaed
matias-inda <indam@udesa.edu.ar>
2024-11-25 18:29:43 -0300

    Home

---
+++ proyectoFinal/src/screens/home.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { auth, db } from "../firebase/config";
+
+class Home extends Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+        };
+    }
+
+    compnentDidMount() {
+      auth.onAuthStateChanged(user => {
+        if (!user) {
+          this.props.navigation.navigate("Login");
+        } else {
+          db.collection("users").where("email", "==", auth.currentUser.email).onSnapshot(snap => {
+            if(!snap.empty) {
+              const data = snap.docs[0];
+              this.setState({ user: data.data().user });
+            }
+          })
+        }
+      })
+      this.fetchPosts();
+    }
+
+    fetchPosts = () => {
+      db.collection("posts").orderBy("createdAt").onSnapshot(query => {
+        const posts = query.docs.map(data => ({
+          id: data.id,
+          data: data.data()
+        }))
+        this.setState({posts: posts})
+      }).catch(e => console.log(e))
+    }
+
+    logout() {
+      auth
+        .signOut()
+        .then(() => {
+          this.props.navigation.navigate("Login");
+        })
+        .catch((error) => console.log(error));
+    }
+
+
+    render() {
+      if (this.state.posts.length === 0) {
+        return (
+            <View>
+                <Text>No hay novedades</Text>
+            </View>
+        );
+    } else {
+        return (
+            <View>
+                <Text>¡Bienvenido!</Text>
+                <Text>Novedades</Text>
+                <Posts />
+            </View>
+        );
+    }
+    }
+  }
+
+export default Home;
